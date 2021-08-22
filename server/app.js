d8367544d6250dbcf7d8cd8725fef124b48d1514
Isuru Nuwanthilaka <isurunuwanthilaka@gmail.com>
2021-08-22 17:12:10 +0530

    add shemas

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect("testdburl");
+
+mongoose.connection.once("open", () => {
+  console.log("Database connected.");
+});
 
 app.use(
   "/graphql",
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,9 @@
 const graphql = require("graphql");
 const _ = require("lodash");
 
+const Book = require("../models/book");
+const Author = require("../models/author");
+
 const {
   GraphQLObjectType,
   GraphQLString,
@@ -75,4 +78,37 @@ const RootQuery = new GraphQLObjectType({
   },
 });
 
-module.exports = new GraphQLSchema({ query: RootQuery });
+const Mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    addAuthor: {
+      type: AuthorType,
+      args: { name: { type: GraphQLString }, age: { type: GraphQLInt } },
+      resolve(parent, args) {
+        let author = new Author({
+          name: args.name,
+          age: args.age,
+        });
+        return author.save();
+      },
+    },
+    addBook: {
+      type: BookType,
+      args: {
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString },
+        authorId: { type: GraphQLID },
+      },
+      resolve(parent, args) {
+        let book = new Book({
+          name: args.name,
+          genre: args.genre,
+          authorId: args.authorId,
+        });
+        return book.save();
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({ query: RootQuery, mutation: Mutation });
